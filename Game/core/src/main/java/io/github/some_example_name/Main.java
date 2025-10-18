c78bd742bae18e52788711887a557df9f3daaa82
MattHoll26 <snk539@york.ac.uk>
2025-10-18 14:09:14 +0100

    Testing

--- Game/core/src/main/java/io/github/some_example_name/Main.java
+++ Game/core/src/main/java/io/github/some_example_name/Main.java
@@ -3,12 +3,14 @@ package io.github.some_example_name;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main implements ApplicationListener {
+public class Main implements ApplicationListener { //anything under this will declare all the variables
     Texture backgroundTexture;
     Texture characterTexture;
     Music music;
@@ -23,7 +25,8 @@ public class Main implements ApplicationListener {
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
 
         spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
+        // tile amounts, each 1 in width or height is equal to 100x100 pixels
+        viewport = new FitViewport(8, 8);
     }
 
     @Override
@@ -31,13 +34,35 @@ public class Main implements ApplicationListener {
         // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
         // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
         if(width <= 0 || height <= 0) return;
+        viewport.update(width, height, true);
 
-        // Resize your application here. The parameters represent the new window size.
     }
 
     @Override
     public void render() {
         // Draw your application here.
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        //spriteBatch.draw(characterTexture, 0, 0, 1, 1); draws the character at position 0,0 with a tile size of 1,1 (it resizes the image to be the tile sizes
+
+        spriteBatch.end();
     }
 
     @Override
