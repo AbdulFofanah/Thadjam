5e58a62bce0492b8d3fa7161e3b5578342d7e21a
Abdul Fofanah <abdulfofanah619@gmail.com>
2025-10-22 10:55:46 +0100

    Adding the extra java files now so we can have modularity

---
+++ Game/core/src/main/java/io/github/some_example_name/Event.java
@@ -0,0 +1,12 @@
+package io.github.some_example_name;
+
+public class Event {
+    public static class HiddenEvent {
+    }
+
+    public static class NegativeEvent {
+    }
+
+    public static class PositiveEvent {
+    }
+}
